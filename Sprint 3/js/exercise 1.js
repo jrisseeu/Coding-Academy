26d11cbe03d933ac4bb676da5cc2b83dde9480d8
jrisseeu <jeffrey.risseeuw@nelnet.net>
2021-04-30 13:16:20 -0500

    Exercise 3

---
+++ Sprint 3/js/exercise 1.js
@@ -0,0 +1,30 @@
+async function weHaveTheMeat() {
+    var numParas = document.getElementById("paras").value;
+    
+    document.getElementById("formatJson").innerHTML = "";
+    document.getElementById("jsonResp").innerHTML = "";
+
+    var apiUrl = "https://baconipsum.com/api/?type=all-meat&paras=" + numParas;
+    
+    var response = await fetch(apiUrl);
+    var myObj = await response.json();
+
+    var jasonObj = JSON.stringify(myObj);
+
+    //clear out the old..  seems to hold the prior.. not sure why
+    document.getElementById("formatJson").innerHTML = "";
+    document.getElementById("jsonResp").innerHTML = "";
+
+    //populate the fields for showing on the screen
+    document.getElementById("jsonResp").innerHTML = jasonObj;
+        
+    for (var para in myObj) {
+        document.getElementById("formatJson").innerHTML +=  "<p>" + myObj[para] + "</p>";
+    }
+
+        
+    return true;
+
+}
+
+

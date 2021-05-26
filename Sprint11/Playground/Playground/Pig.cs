97ba119c3d72c45b5955d9032ccf1ad270b9e829
jrisseeu <jeffrey.risseeuw@nelnet.net>
2021-05-26 09:42:38 -0500

    Adding code

---
+++ Sprint11/Playground/Playground/Pig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playground {
+    class Pig : Animal   {
+
+        public override void makesSound() {
+
+            Console.WriteLine("Oink Oink");
+        }
+
+
+    }
+}

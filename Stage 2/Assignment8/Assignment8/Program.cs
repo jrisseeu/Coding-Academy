5c2deb8acdd1c36fec60de7a6c62b5f8f2f7de87
jrisseeu <jeffrey.risseeuw@nelnet.net>
2021-06-25 09:09:45 -0500

    new code and move of code to a different folder

---
+++ Stage 2/Assignment8/Assignment8/Program.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    static class Program
+    {
+        public static IConfigurationRoot _configuration { get; private set; }
+
+        static async Task Main(string[] args)  {
+            using IHost host = CreateHostBuilder(args).Build(); await host.RunAsync();
+        }
+        static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext,configuration) => {
+                configuration.Sources.Clear();
+                configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                IConfigurationRoot configurationRoot = configuration.Build();
+                _configuration = configurationRoot;
+            }).ConfigureServices((services) => {
+                services.AddHostedService<ConsoleService>();
+            });
+
+        static Contact[] ReadContacts() {
+            using var reader = new StreamReader("Contacts.csv"); 
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture); 
+            var contacts = csv.GetRecords<Contact>().ToArray(); return contacts;
+        }
+
+
+    }
+}

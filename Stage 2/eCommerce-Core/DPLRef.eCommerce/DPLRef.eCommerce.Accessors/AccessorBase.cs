67ff2e21e6256cb47ef3f9c641470144c89d640c
jrisseeu <jeffrey.risseeuw@nelnet.net>
2021-06-29 13:39:19 -0500

    Adding to version control

---
+++ Stage 2/eCommerce-Core/DPLRef.eCommerce/DPLRef.eCommerce.Accessors/AccessorBase.cs
@@ -0,0 +1,8 @@
+﻿using DPLRef.eCommerce.Common.Shared;
+using DPLRef.eCommerce.Utilities;
+
+namespace DPLRef.eCommerce.Accessors {
+    abstract class AccessorBase : ServiceContractBase {
+        public UtilityFactory UtilityFactory { get; set; }
+    }
+}
---
+++ Stage 2/eCommerce-Core/DPLRef.eCommerce/DPLRef.eCommerce.Engines/Sales/TaxCalculationEngine.cs
@@ -0,0 +1,30 @@
+﻿using DPLRef.eCommerce.Accessors.Sales;
+using DPLRef.eCommerce.Contracts.WebStore.Sales;
+using System;
+
+namespace DPLRef.eCommerce.Engines.Sales
+{
+    class TaxCalculationEngine : EngineBase, ITaxCalculationEngine
+    {
+        public override string TestMe(string input)
+        {
+            input = base.TestMe(input);
+            return input;
+        }
+
+        public WebStoreCart CalculateCartTax(WebStoreCart cart) {
+
+            if (cart != null && cart.BillingAddress != null && !string.IsNullOrWhiteSpace(cart.BillingAddress.Postal)) {
+                var taxRate = AccessorFactory.CreateAccessor<ITaxRateAccessor>().Rate(cart.BillingAddress);
+
+                foreach (var item in cart.CartItems) {
+                    cart.TaxAmount += Math.Round(item.ExtendedPrice * taxRate, 2); 
+                }
+            }
+            // update the cart total with the tax amount
+            cart.Total += Math.Round(cart.TaxAmount, 2);
+
+            return cart;
+        }
+    }
+}
